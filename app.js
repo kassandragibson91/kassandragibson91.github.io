7d3fc4ae2316a9adfaf198ffca89922d23061ec1
Kassandra Leigh Gibson <kgillmore0001@kctcs.edu>
2026-04-14 08:14:02 -0500

    Add function to fetch and display student data
    Implement async function to fetch and display student data based on selected major, with error handling.

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// app.js code coming soon 
+async function getData(selected_major) {
+  var response = await fetch('cit5students.json');
+
+  if (response.ok) {
+    var data = await resonse.json();
+    var student_items = data.filter((item) => item.major == selected_major);
+    var templateText = document.getElementById('studentTemplate').innerHTML;
+    var compiledTemplate = Handlebars.compile(templateText);
+    var compiledHtml = compiledTemplate({ rows: student_items });
+    document.getElementById('studentTable').innerHTML = comiledHtml;
+  }
+  else
+  {
+    document.querySelector('#studentTable').innerHTML = "There was an error, or student data not found.";
+  }
+}
